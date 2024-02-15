f3ec007900612d7eb2e3f7afb3defa092df61c0d
Kevin Vu <kevinvu59@gmail.com>
2024-02-15 12:55:45 -0500

    Add initial file setup. Also add xcshareddata to gitignore

---
+++ BrainLabs/BrainLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainLabsApp.swift
+//  BrainLabs
+//
+//  Created by Kevin Vu on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
