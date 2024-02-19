4b58226c8caf9ac9db31483bf5635b46d66e0808
Kevin Vu <50222947+notkevinvu@users.noreply.github.com>
2024-02-19 18:05:59 -0500

    Add core tab view and the home/games/profile views and swinject (#1)

--- BrainLabs/BrainLabsApp.swift
+++ BrainLabs/BrainLabsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BrainLabsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CoreTabView()
         }
     }
 }
---
+++ BrainLabs/FeatureDomain/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  BrainLabs
+//
+//  Created by Kevin Vu on 2/19/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile view")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
