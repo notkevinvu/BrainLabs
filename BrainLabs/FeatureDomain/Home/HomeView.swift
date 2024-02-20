bec6e75658e119a9cdf988d6e0efd0233e2e7344
Kevin Vu <kevinvu59@gmail.com>
2024-02-20 15:46:44 -0500

    Implement rough structure for home page

--- BrainLabs/FeatureDomain/Home/HomeView.swift
+++ BrainLabs/FeatureDomain/Home/HomeView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home view")
+        ScrollView(.vertical) {
+            VStack(spacing: 20) {
+                Text("Brain Lab")
+                    .font(.largeTitle.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                // TODO: Make this text copy a card?
+                Text("Welcome back! See your recent games, try something new, or quickly see your highlights!")
+                    .font(.callout)
+                
+                carouselSection(title: "Recents")
+                carouselSection(title: "Try something new")
+            } // vstack
+            .padding(20)
+        } // scrollview
+    }
+    
+    @ViewBuilder
+    func carouselSection(title: String) -> some View {
+        VStack(spacing: 10) {
+            Text(title)
+                .font(.title.bold())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            LargeCardCarouselView()
+                .frame(minHeight: 225)
+        }
     }
 }
 
