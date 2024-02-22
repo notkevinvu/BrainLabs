ea1421903e95662a380e2d883faf8c56179d7d28
Kevin Vu <kevinvu59@gmail.com>
2024-02-22 14:46:42 -0500

    Move mock models to view model

--- BrainLabs/FeatureDomain/Home/HomeViewModel.swift
+++ BrainLabs/FeatureDomain/Home/HomeViewModel.swift
@@ -13,3 +13,18 @@ final class HomeViewModel {
     
     
 }
+#warning("Remove mock model and data, add specific colors to each game when creating actual models")
+struct GameModelMock: Identifiable {
+    var id = UUID()
+    var title: String
+    var subtitle: String
+    
+    let placeholderImage = Image(systemName: "brain.filled.head.profile")
+}
+
+var mockData: [GameModelMock] = [
+    .init(title: "Aim trainer", subtitle: "Test your hand eye coordination and reaction."),
+    .init(title: "Chimp test", subtitle: "Monkey see, monkey do."),
+    .init(title: "Texting", subtitle: "How fast can you type up that story to your friend?"),
+    .init(title: "Number memory", subtitle: "In this case, bigger is better.")
+]
