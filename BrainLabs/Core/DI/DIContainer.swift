ba1d57a6a191f4511851e9be4d76265c9e6bbd1a
Kevin Vu <kevinvu59@gmail.com>
2024-03-11 14:14:04 -0400

    Add DI container and registration methods

---
+++ BrainLabs/Core/DI/DIContainer.swift
@@ -0,0 +1,60 @@
+//
+//  DIContainer.swift
+//  BrainLabs
+//
+//  Created by Kevin Vu on 3/11/24.
+//
+
+import Foundation
+import Swinject
+
+/// A singleton class responsible for injecting dependencies within the app.
+final class DIContainer {
+    
+    static let shared = DIContainer()
+    
+    private init() {}
+    
+    private var container = Container()
+    
+    @MainActor
+    func injectModules() {
+        injectServices()
+        injectProviders()
+        injectViewModels()
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T {
+        // Crashing here is good as it means we have not correctly
+        // injected/registered the specific dependency and can then fix.
+        container.resolve(T.self)!
+    }
+}
+
+extension DIContainer {
+    // MARK: -- Injecting managers/services
+    
+    private func injectServices() {
+        // no managers at the moment
+    }
+    
+    // MARK: -- Injecting providers
+    
+    private func injectProviders() {
+        container.register(RecommendationsProvider.self) { resolver in
+            return RecommendationsProviderImpl()
+        }
+        .inObjectScope(.container)
+    }
+    
+    // MARK: -- Injecting view models
+    
+    private func injectViewModels() {
+        container.register(HomeViewModel.self) { resolver in
+            return HomeViewModel(
+                recommendationsProvider: resolver.resolve(RecommendationsProvider.self)!
+            )
+        }
+        .inObjectScope(.transient)
+    }
+}
